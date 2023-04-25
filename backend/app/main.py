0cf34fe035715eadd9340f7f683f4fa324b017ee
Ghazal89 <ghazaleh.hadianghahfarokhi001@stud.fh-dortmund.de>
2023-04-25 22:39:39 +0200

    final

--- backend/app/main.py
+++ backend/app/main.py
@@ -6,9 +6,10 @@ from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 
 
-
 app = FastAPI()
-origins = ["http://localhost:3000",]
+origins = [
+    "http://localhost:3000",
+]
 
 # add middleware
 app.add_middleware(
@@ -26,10 +27,13 @@ class InputData(BaseModel):
 
 @app.post("/predict")
 async def upload(file: InputData):
-    true_base64_file = file.data.split(",", 1)[1].encode("ascii")
-    decoded_image = base64.b64decode(true_base64_file)
-    img_file = open("uploadImage.jpg", "wb")
-    img_file.write(decoded_image)
-    img_file.close()
-    classification_label: str = predict("uploadImage.jpg")
-    return {"status": "ok", "image": file.data, "class_label": classification_label}
+    try:
+        true_base64_file = file.data.split(",", 1)[1].encode("ascii")
+        decoded_image = base64.b64decode(true_base64_file)
+        img_file = open("uploadImage.jpg", "wb")
+        img_file.write(decoded_image)
+        img_file.close()
+        classification_label: str = predict("uploadImage.jpg")
+        return {"status": "ok", "image": file.data, "class_label": classification_label}
+    except:
+        return {"status": "error"}
